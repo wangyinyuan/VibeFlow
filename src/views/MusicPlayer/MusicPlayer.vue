51a7428d365f440d25ff13243525fa50f0505513
BigWang <2972437973@qq.com>
2023-10-19 19:06:11 +0800

    add basic music player card

--- src/views/MusicPlayer/MusicPlayer.vue
+++ src/views/MusicPlayer/MusicPlayer.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="music-player-container"></div>
+  <div class="music-player-container">
+    <!-- 歌词 -->
+    <div class="lyrics-container">What was I made for</div>
+    <!-- MusicPlayerCard 音乐播放器主体 -->
+    <main class="music-player-card-container">
+      <!-- Card -->
+      <div class="music-player-card">
+        <img src="@/assets/musicCover/cover-1.jpg" alt="the album cover" class="album-cover" />
+      </div>
+    </main>
+  </div>
 </template>
 
 <style lang="scss" scoped module>
@@ -9,5 +19,7 @@
   background: url('@/assets/bgPicture/cool-background(2).png') no-repeat center / cover;
   height: 100vh;
   width: auto;
+
+  //album cover
 }
 </style>

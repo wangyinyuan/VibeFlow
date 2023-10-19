31044f6266234bdb61e09e503fd473ebdbb62059
BigWang <2972437973@qq.com>
2023-10-19 23:52:33 +0800

    增加底部播放器卡片边框样式

--- src/views/MusicPlayer/MusicPlayer.vue
+++ src/views/MusicPlayer/MusicPlayer.vue
@@ -8,18 +8,45 @@
     <main class="music-player-card-container">
       <!-- Card -->
       <div class="music-player-card">
-        <img src="@/assets/musicCover/cover-1.jpg" alt="the album cover" class="album-cover" />
+        <div class="card-left">
+          <img src="@/assets/musicCover/cover-1.jpg" alt="the album cover" class="album-cover" />
+        </div>
+
+        <div class="card-right"></div>
       </div>
     </main>
   </div>
 </template>
 
-<style lang="scss" scoped module>
+<style lang="scss" scoped>
 .music-player-container {
+  position: relative;
+
   background: url('@/assets/bgPicture/cool-background(2).png') no-repeat center / cover;
   height: 100vh;
-  width: auto;
+  width: 100vw;
 
-  //album cover
+  .music-player-card-container {
+    position: absolute;
+    bottom: 1rem;
+    left: calc(50% - 13rem);
+    .music-player-card {
+      position: relative;
+      width: 26rem;
+      height: 7rem;
+      border-radius: 1rem;
+      background: #e0e0e0;
+
+      //album cover
+      .album-cover {
+        position: absolute;
+        top: -1rem;
+        left: 2rem;
+        height: 6rem;
+        width: 6rem;
+        border-radius: 1rem;
+      }
+    }
+  }
 }
 </style>

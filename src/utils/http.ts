73a62e24dd366db6669570187db0be94cb2a1dd5
BigWang <2972437973@qq.com>
2023-10-24 12:26:31 +0800

    修复axios实例的异步bug

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,48 +1,50 @@
-import { RealIp } from './../types/realIP.d'
+import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
-import type { AxiosRequestConfig } from 'axios'
+import { RealIp } from './../types/realIP.d'
 
 const httpInstance = axios.create({
   baseURL: 'https://netease-cloud-music-api-liard-mu-78.vercel.app/',
   timeout: 5000,
   withCredentials: true
 })
-
-//获取用户的真实IP
-let realIP: string
-axios
-  .get<RealIp>('https://api.ipify.org?format=json')
-  .then(res => {
+async function setupInterceptors() {
+  //获取用户的真实IP
+  let realIP: string
+  try {
+    const res = await axios.get<RealIp>('https://api.ipify.org?format=json')
     realIP = res.data.ip
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error occurred: ', error)
-  })
-//请求拦截器
-httpInstance.interceptors.request.use(
-  config => {
-    config.params.realIP = realIP
-    return config
-  },
-  e => {
-    return Promise.reject(e)
+    realIP = '163.123.192.53'
   }
-)
-//响应拦截器
-httpInstance.interceptors.response.use(
-  response => {
-    return response.data
-  },
-  error => {
-    ElMessage({
-      type: 'error',
-      message: error.response.data.message,
-      duration: 2000
-    })
-    return Promise.reject(error)
-  }
-)
+  //请求拦截器
+  httpInstance.interceptors.request.use(
+    config => {
+      config.params.realIP = realIP
+      return config
+    },
+    e => {
+      return Promise.reject(e)
+    }
+  )
+  //响应拦截器
+  httpInstance.interceptors.response.use(
+    response => {
+      return response.data
+    },
+    error => {
+      ElMessage({
+        type: 'error',
+        message: error.response.data.message,
+        duration: 2000
+      })
+      return Promise.reject(error)
+    }
+  )
+}
+
+setupInterceptors()
 
 export function request<T = any>(config: AxiosRequestConfig): Promise<T> {
   return httpInstance(config) as Promise<T>
